b5181695411fdd2457c9d4eaaf88b16f0272df52
Elena <varlamova.elena200@gmail.com>
2021-10-12 18:20:01 +0300

    Line rotate 90 degrees

--- AffineTransformations/LineTransitions.cs
+++ AffineTransformations/LineTransitions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,17 @@ namespace AffineTransformations
     {
         private void buttonRotateLine_Click(object sender, EventArgs e)
         {
+           // buttonСhooseCentre.Visible = false;
+            textAngle.Text = "90";//по условию, повернуть ребро вокруг центра на 90 градусов
+            //координата центра отрезка
+            Point p1 = polygonPoints[0];
+            Point p2 = polygonPoints[1];
+            Point pnew = new Point((p1.X + p2.X) / 2, (p1.Y + p2.Y) / 2);//ищем центр отрезка
+            rotateAroundPoint(pnew, canvas.Size);//поворачиваем
+            g.Clear(Color.White);
+            g.DrawLine(blackPen,polygonPoints[0], polygonPoints[1]);
+            textAngle.Clear();//для последующей работы
+
 
         }
 
